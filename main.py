2f5ebf1411bcd67e34f4f68d34e4ef48c0876a3a
arif <arifur.tuc@gmail.com>
2023-11-14 22:27:12 +0100

    Add table

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 # Iterate through each file path in the 'filepaths' list
 for filepath in filepaths:
-    # Read data from the Excel file into a Pandas DataFrame
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     # Create a new PDF document
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -26,7 +23,38 @@ for filepath in filepaths:
 
     # Add date to the PDF
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    # Read data from the Excel file into a Pandas DataFrame
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Convert column names to a more readable format
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+
+    # Set font, size, and text color for the table headers
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+
+    # Create table headers in the PDF
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=65, h=8, txt=columns[1], border=1)
+    pdf.cell(w=35, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Iterate over DataFrame rows and populate the PDF with data
+    for index, row in df.iterrows():
+        # Set font and text color for the table data
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+
+        # Add data to the PDF table cells
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=65, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     # Output the PDF with the filename derived from the original file
     pdf.output(f"PDFs/{filename}.pdf")
